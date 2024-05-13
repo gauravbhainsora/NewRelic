02bcf3dfd0b252eddea4d037ed9210804fb554e8
Gaurav Bhainsora <gauravv.bhainsora@gmail.com>
2024-05-13 15:35:20 +0530

    add

--- visualizations/jhallad-mal/index.js
+++ visualizations/jhallad-mal/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { AreaChart, LineChart, PieChart } from "nr1";
+import { AreaChart, LineChart, PieChart,BarChart } from "nr1";
 import { Grid, GridItem } from "nr1";
 
 const index = () => {
@@ -24,6 +24,13 @@ const index = () => {
         </GridItem>
         <GridItem columnSpan={6}>
           <PieChart
+            accountId={4438261}
+            query="SELECT average(transmitBytesPerSecond) AS `Transmit bytes per second`, average(receiveBytesPerSecond) AS `Receive bytes per second` FROM NetworkSample WHERE (entityGuid = 'NDQzODI2MXxJTkZSQXxOQXw5MDY2NzExMjg0MTc4NTQwMDM4') TIMESERIES AUTO"
+            fullWidth
+          />
+        </GridItem>
+        <GridItem columnSpan={6}>
+          <BarChart
             accountId={4438261}
             query="SELECT average(transmitBytesPerSecond) AS `Transmit bytes per second`, average(receiveBytesPerSecond) AS `Receive bytes per second` FROM NetworkSample WHERE (entityGuid = 'NDQzODI2MXxJTkZSQXxOQXw5MDY2NzExMjg0MTc4NTQwMDM4') TIMESERIES AUTO"
             fullWidth
